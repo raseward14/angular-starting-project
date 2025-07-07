1225e32f0489f1cacecf5babfdd8306186b14299
raseward14 <raseward14@gmail.com>
2025-07-06 20:20:06 -0600

    filled out decorator for custom header component

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    
+    selector: 'richard-header',
+    standalone: true,
+    templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
