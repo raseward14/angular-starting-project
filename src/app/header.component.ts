f053ee045969b13a32e1e8d74bdc033cf32c7a6c
raseward14 <raseward14@gmail.com>
2025-07-06 20:20:25 -0600

    completed first custom component - the header!

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
     selector: 'richard-header',
     standalone: true,
     templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {}
