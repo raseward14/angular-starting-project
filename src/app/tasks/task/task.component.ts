3d190fb0625d5119911b065ee6530675e7b7d279
raseward14 <raseward14@gmail.com>
2025-07-13 15:46:36 -0600

    created task input, and task type, in task component, to display actual task data, using string interpolation and property binding to pass the given task to the custom app task element, in the @for loop

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+type Task = {
+  id: string,
+  userId: string,
+  title: string,
+  summary: string,
+  dueDate: string,
+}
 
 @Component({
   selector: 'app-task',
@@ -7,4 +15,7 @@ import { Component } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
-export class TaskComponent {}
+
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+}
