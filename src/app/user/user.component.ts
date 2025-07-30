e734e9f3385b6412a4d720ba4d16ecb10bfd308f
raseward14 <raseward14@gmail.com>
2025-07-29 18:13:42 -0600

    final commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,7 +14,7 @@ export class UserComponent {
   @Input({ required: true }) user!: User;
   @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
- 
+
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
