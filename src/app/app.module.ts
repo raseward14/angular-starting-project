4a60ce14d42cb5893a9f9c7ebf66a2effdfb0019
raseward14 <raseward14@gmail.com>
2025-07-31 16:19:15 -0600

    updated main.ts and app module for modular based application startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule} from '@angular/core';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: []
+    declarations: [AppComponent]
 })
 export class AppModule {}
