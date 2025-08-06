1f9f09ab51ac9b7adfb6991a8ee9a05cae21626d
raseward14 <raseward14@gmail.com>
2025-08-06 17:41:10 -0600

    created tasks module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { SharedModule } from './ui/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, TasksComponent, TaskComponent, NewTaskComponent, UserComponent],
+    declarations: [AppComponent, HeaderComponent,  UserComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule, SharedModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule { }
